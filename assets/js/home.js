a26bb024b8d938236551cacbfc10e3d564c013aa
SushantDaga <sushantdaga@Sushant-Daga.local>
2020-10-06 19:37:00 +0530

    trying something

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,13 +2,10 @@ $(function() {
   /* NOTE: hard-refresh the browser once you've updated this */
   $(".typed").typed({
     strings: [
-      "stat rish.human<br/>" + 
-      "><span class='caret'>$</span> skills: android, hardware-software interaction, camera, computer vision<br/> ^100" +
-      "><span class='caret'>$</span> job: android auto at <a href='http://www.google.com/'>Google Munich</a><br/> ^100" +
-      "><span class='caret'>$</span> hobbies: football, travel, <a href='http://www.rish.space/blog'>writing</a><br/> ^300" +
-      "><span class='caret'>$</span> alias: crearo <br/>" +
-      "><span class='caret'>$</span> highlight:  <a href='/projects/video-stab'>real time image stabilization on Android</a>, <a href='/projects/lifehacks'>app with >750K installs</a><br/>"/*
-      "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/crearo/'>github</a> <a href='http://in.linkedin.com/in/bhardwajrish/'>linkedin</a> <a href='http://bhardwajrish.blogspot.com/'>blog</a><br/>"*/
+      "stat sushant.daga<br/>" + 
+      "><span class='caret'>$</span> skills: Deep Learning, Natural language Processing, Research-oriented, Python, AWS, SQL ...<br/> ^100" +
+      "><span class='caret'>$</span> job: Data Scientist at <a href='https://www.vmock.com/'>Vmock</a><br/> ^100" +
+      "><span class='caret'>$</span> hobbies: Reading, Dancing, Writing <a href='/writing'>writing</a><br/> ^300"
     ],
     showCursor: true,
     cursorChar: '_',
