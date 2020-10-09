4ee236f934f2ff014c38704dc28ea729a10e207f
SushantDaga <sushantdaga@Sushant-Daga.local>
2020-10-09 11:13:43 +0530

    trying something

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,7 +5,7 @@ $(function() {
       "stat sushant.daga<br/>" + 
       "><span class='caret'>$</span> skills: Deep Learning, Natural Language Processing, Computer Vision, Python, AWS, SQL ...<br/> ^100" +
       "><span class='caret'>$</span> job: Data Scientist at <a target='_blank' href='https://www.vmock.com/'>Vmock</a><br/> ^100" +
-      "><span class='caret'>$</span> hobbies: Reading, Dancing, <a href='/writing'>Writing</a>, Badminton, TT<br/> ^300" +
+      "><span class='caret'>$</span> hobbies: Reading, Dancing, <a target='_blank' href='/writing'>Writing</a>, Badminton, TT<br/> ^300" +
       "><span class='caret'>$</span> miscellaneous: Research &amp; Data oriented, Experiment ready, Business minded <br/>"
     ],
     showCursor: true,
